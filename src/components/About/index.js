33edc2a9f699d920457197a14d9195c1f037870f
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-13 18:40:34 -0700

    add id to About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function About() {
   return (
-    <p> 
+    <p id='about-me'> 
       Hello! My name is Kaia. I am a 22-year-old full stack web developer residing in Eugene, Oregon.
       My front end skills include HTML, CSS, Javascript, React, Handlebars, Bootstrap, jQuery, and AJAX.
       My back end skills include Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and GraphQL.
