c0623753342d94ecece112a114ede969d84ba393
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-13 18:17:28 -0700

    create outline for Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+      <div id='footer'>
+        <a href='https://github.com/kaiafay'>Github</a>
+        <a href='https://www.linkedin.com/in/kaia-scheirman-98708522b/'>LinkedIn</a>
+        <a href='https://www.instagram.com/kaiacodes/'>Instagram</a>
+      </div>
+    )
+}
+
+export default Footer;
