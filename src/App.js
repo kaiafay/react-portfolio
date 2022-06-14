55d3bda567609df717115ac81e950c8ae22d7a36
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-13 18:54:39 -0700

    create outline for Resume component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Resume from './components/Resume';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Nav></Nav>
       <About></About>
+      <Resume></Resume>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/Resume/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Resume() {
+    return (
+      <section id='resume'>
+        <p>
+            Front-End Proficiencies:
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Handlebars</li>
+                <li>Bootstrap</li>
+                <li>jQuery</li>
+                <li>AJAX</li>
+            </ul>
+
+            Back-End Proficiencies:
+            <ul>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>MySQL</li>
+                <li>Sequelize</li>
+                <li>MongoDB</li>
+                <li>Mongoose</li>
+                <li>GraphQL</li>
+            </ul>
+
+            <a href='https://docs.google.com/document/d/1YV2tY_aoCaD5SaVKfj3n92SHHAVopVdtTMKQ1RyjXww/edit?usp=sharing' rel='noreferrer' target='_blank'>View My Resume Here</a>
+        </p>
+      </section>
+    )
+}
+
+export default Resume;
