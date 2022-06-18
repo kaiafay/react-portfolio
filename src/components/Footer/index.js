b0d985a94565e265c34e8941fb6178a5ac6e19ec
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-18 15:29:39 -0700

    initial styling of footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Footer() {
     return (
       <div id='footer'>
-        <a href='https://github.com/kaiafay'>Github</a>
-        <a href='https://www.linkedin.com/in/kaia-scheirman-98708522b/'>LinkedIn</a>
-        <a href='https://www.instagram.com/kaiacodes/'>Instagram</a>
+        <a href='https://github.com/kaiafay' target='_blank' rel='noreferrer'>Github</a>
+        <a href='https://www.linkedin.com/in/kaia-scheirman-98708522b/' target='_blank' rel='noreferrer'>LinkedIn</a>
+        <a href='https://www.instagram.com/kaiacodes/' target='_blank' rel='noreferrer'>Instagram</a>
       </div>
     )
 }
