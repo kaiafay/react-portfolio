f0d735ae94b03d93ef35e379e2110769b6aa0cd7
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-17 19:06:38 -0700

    add avatar photo to about me page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Avatar from '../../assets/images/avatar.JPG'
 
 function About() {
   return (
-    <p id='about-me'> 
-      Hello! My name is Kaia. I am a 22-year-old full stack web developer residing in Eugene, Oregon.
-      My front end skills include HTML, CSS, Javascript, React, Handlebars, Bootstrap, jQuery, and AJAX.
-      My back end skills include Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and GraphQL.
-      I am a fast learner with an exceptional eye for detail who specializes in the MERN stack.
-    </p>
+    <div>
+      <img src={Avatar} alt='avatar of kaia' />
+      <p id='about-me'> 
+        Hello! My name is Kaia. I am a 22-year-old full stack web developer residing in Eugene, Oregon.
+        My front end skills include HTML, CSS, Javascript, React, Handlebars, Bootstrap, jQuery, and AJAX.
+        My back end skills include Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and GraphQL.
+        I am a fast learner with an exceptional eye for detail who specializes in the MERN stack.
+      </p>
+    </div>
   )
 }
 
