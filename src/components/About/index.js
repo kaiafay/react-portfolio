d12b3745b246b0c3612bdc9fb9fd58ed83e7e915
Kaia Scheirman <kfscheirman18@gmail.com>
2022-09-12 16:06:11 -0700

    update avatar photo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import Avatar from '../../assets/images/avatar.JPG'
+import React from "react";
+import Avatar from "../../assets/images/avatar.jpg";
 
 function About() {
   return (
-    <div className='about'>
-      <img id='avatar' src={Avatar} alt='avatar of kaia' />
-      <p id='about-me'> 
-        Hello! My name is Kaia. I am a full stack developer residing in Eugene, Oregon.
-        I have a passion for sleek, intuitive designs.
-        My front end skills include HTML, CSS, Javascript, React, Handlebars, Bootstrap, jQuery, and AJAX.
-        My back end skills include Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and GraphQL.
-        I am a fast learner with an exceptional eye for detail who specializes in the MERN stack.
+    <div className="about">
+      <img id="avatar" src={Avatar} alt="avatar of kaia" />
+      <p id="about-me">
+        Hello! My name is Kaia. I am a full stack developer residing in Eugene,
+        Oregon. I have a passion for sleek, intuitive designs. My front end
+        skills include HTML, CSS, Javascript, React, Handlebars, Bootstrap,
+        jQuery, and AJAX. My back end skills include Node.js, Express.js, MySQL,
+        Sequelize, MongoDB, Mongoose, and GraphQL. I am a fast learner with an
+        exceptional eye for detail who specializes in the MERN stack.
       </p>
     </div>
-  )
+  );
 }
 
 export default About;
